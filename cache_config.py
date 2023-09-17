bb10f8546c83c1d740592971f318b41b35d513e5
rilesdun <rilydunlap@outlook.com>
2023-09-17 21:19:19 +0000

    clean branch for github pages

--- cache_config.py
+++
@@ -1,6 +0,0 @@
-"""
-Cache configuration for the application.
-"""
-from flask_caching import Cache
-
-cache = Cache(config={'CACHE_TYPE': 'simple'})
--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-"""
-Gunicorn config file
-"""
-
-import multiprocessing
-
-bind = "0.0.0.0:5000"  # pylint: disable=invalid-name
-worker_class = 'eventlet' # pylint: disable=invalid-name
-workers = multiprocessing.cpu_count() * 2 + 1
